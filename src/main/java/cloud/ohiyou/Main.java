5f18093bdb826a007f8652a754f6cbb893bd31e3
刘豪 <ohiyou999@163.com>
2024-02-21 10:53:59 +0800

    初始化提交

---
+++ src/main/java/cloud/ohiyou/Main.java
@@ -0,0 +1,11 @@
+package cloud.ohiyou;
+
+/**
+ * @author ohiyou
+ * @since ${DATE} ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
