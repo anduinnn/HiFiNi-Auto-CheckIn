05294da2ea4f00c64d022ab6975e6d3a99a320f2
刘豪 <ohiyou999@163.com>
2024-02-22 09:32:40 +0800

    格式化代码;修改签到成功/失败后状态错误问题

--- src/main/java/cloud/ohiyou/Main.java
+++ src/main/java/cloud/ohiyou/Main.java
@@ -10,88 +10,84 @@ import org.apache.http.impl.client.HttpClients;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
-/**
- * @author ohiyou
- * @since ${DATE} ${TIME}
- */
 public class Main {
-    public static void main(String[] args) throws IOException {
-        // 记录开始时间
-        long startTime = System.nanoTime();
+    public static void main(String[] args) {
+        try {
+            long startTime = System.nanoTime();
 
-        // 创建HttpClient实例
-        CloseableHttpClient httpClient = HttpClients.createDefault();
+            CloseableHttpClient httpClient = HttpClients.createDefault();
 
-        // 获取环境变量
-        String cookieValue = System.getenv("COOKIE");
-        String serverChanKey = System.getenv("SERVER_CHAN");
+            String cookieValue = System.getenv("COOKIE");
+            String serverChanKey = System.getenv("SERVER_CHAN");
 
-        if (cookieValue == null) {
-            publishWechat(httpClient, serverChanKey, new ResultVO(-1, "COOKIE 环境变量未设置"), null);
-            throw new RuntimeException("COOKIE 环境变量未设置");
+            if (cookieValue == null) {
+                handleEnvironmentError(httpClient, serverChanKey, new ResultVO(-1, "COOKIE 环境变量未设置"));
+                throw new RuntimeException("COOKIE 环境变量未设置");
+            }
+
+            HttpResponse response = sendSignInRequest(httpClient, cookieValue);
+
+            long endTime = System.nanoTime();
+            Long duration = (endTime - startTime) / 1000000;
+
+            String result = readResponse(response);
+
+            System.out.println(result);
+            ResultVO resultVO = toResultVO(result);
+
+            publishWechat(httpClient, serverChanKey, resultVO, duration);
+
+            httpClient.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
 
-        // 发送签到请求
+    private static void handleEnvironmentError(CloseableHttpClient httpClient, String serverChanKey, ResultVO resultVO) {
+        System.out.println("SERVER_CHAN 环境变量未设置");
+        publishWechat(httpClient, serverChanKey, resultVO, null);
+    }
+
+    private static HttpResponse sendSignInRequest(CloseableHttpClient httpClient, String cookieValue) throws IOException {
         HttpPost httpPost = new HttpPost("https://www.hifini.com/sg_sign.htm");
         httpPost.setHeader("Cookie", cookieValue);
         httpPost.setHeader("X-Requested-With", "XMLHttpRequest");
         httpPost.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36");
 
-        HttpResponse response = httpClient.execute(httpPost);
+        return httpClient.execute(httpPost);
+    }
 
-        // 执行结束时间
-        long endTime = System.nanoTime();
-
-        // 执行时间
-        Long duration = (endTime - startTime) / 1000000;
-
-        // 读取响应
+    private static String readResponse(HttpResponse response) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+        StringBuilder result = new StringBuilder();
         String readLine;
-        StringBuffer result = new StringBuffer();
         while ((readLine = reader.readLine()) != null) {
             result.append(readLine);
         }
-
-        System.out.println(result);
-        ResultVO resultVO = toResultVO(result);
-
-        // 发送微信推送
-        publishWechat(httpClient, serverChanKey, resultVO, duration);
-
-        // 关闭client
-        httpClient.close();
+        return result.toString();
     }
 
-    private static ResultVO toResultVO(StringBuffer result) {
-        return JSONObject.parseObject(result.toString(), ResultVO.class);
+    private static ResultVO toResultVO(String result) {
+        return JSONObject.parseObject(result, ResultVO.class);
     }
 
-    public static void publishWechat(CloseableHttpClient httpClient, String serverChanKey, ResultVO resultVO, Long duration) {
+    private static void publishWechat(CloseableHttpClient httpClient, String serverChanKey, ResultVO resultVO, Long duration) {
         if (serverChanKey == null) {
             System.out.println("SERVER_CHAN 环境变量未设置");
             return;
         }
-        String title;
-        if (resultVO.getCode() == -1) {
-            title = "HiFiNi签到失败";
-        } else {
-            title = "HiFiNi签到成功";
-        }
+
+        String title = (resultVO.getMessage().contains("成功签到")) ? "HiFiNi签到成功" : "HiFiNi签到失败";
 
         if (duration != null) {
-            title += "，耗时" + duration + "ms";
+            title += "，耗时 " + duration + "ms";
         }
-        HttpGet httpGet = null;
+
         try {
-            httpGet = new HttpGet("https://sctapi.ftqq.com/" + serverChanKey + ".send?title=" + URLEncoder.encode(title, "UTF-8") + "&desp=" + URLEncoder.encode(resultVO.getMessage()));
-        } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException(e);
-        }
-        try {
+            HttpGet httpGet = new HttpGet("https://sctapi.ftqq.com/" + serverChanKey + ".send?title=" +
+                    URLEncoder.encode(title, "UTF-8") + "&desp=" + URLEncoder.encode(resultVO.getMessage(), "UTF-8"));
             httpClient.execute(httpGet);
         } catch (IOException e) {
             e.printStackTrace();
