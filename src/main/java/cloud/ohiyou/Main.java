d6b513c4b853e5564f8e945c4a37660263003c3f
刘豪 <ohiyou999@163.com>
2024-03-01 17:12:57 +0800

    过滑动验证码;发送请求修改为 okhttp

--- src/main/java/cloud/ohiyou/Main.java
+++ src/main/java/cloud/ohiyou/Main.java
@@ -5,11 +5,10 @@ package cloud.ohiyou;
  * @since 2024/3/1 14:19
  */
 
-import cloud.ohiyou.vo.*;
 import cloud.ohiyou.vo.Cookie;
+import cloud.ohiyou.vo.*;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import com.sun.deploy.net.HttpResponse;
 import okhttp3.*;
 
 import java.io.BufferedReader;
