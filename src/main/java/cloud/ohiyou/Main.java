fadb41fa460fcc7dbd3247ebaca9f2f83df2e5f8
刘豪 <ohiyou999@163.com>
2024-03-02 17:34:21 +0800

    运行结束之后手动关闭httpClient,防止线程无法正常终止

--- src/main/java/cloud/ohiyou/Main.java
+++ src/main/java/cloud/ohiyou/Main.java
@@ -25,8 +25,8 @@ public class Main {
     public static final String CLIENT_ID = "CAP-28F1B67C27C8F7DF6F68C7DD69895A6C";
     public static final String CAPTCHA_ID = "9464902a3345d323ed58bde565f260ee";
     public static String SI_SIGN = "";
-    private static final String COOKIE = System.getenv("COOKIE");
-    //    private static final String COOKIE = "";
+    //    private static final String COOKIE = System.getenv("COOKIE");
+    private static final String COOKIE = "";
     private static final String SERVER_CHAN_KEY = System.getenv("SERVER_CHAN");
     private static final OkHttpClient client = new OkHttpClient.Builder()
             .connectTimeout(30, TimeUnit.SECONDS)
@@ -54,9 +54,10 @@ public class Main {
 
             publishWechat(SERVER_CHAN_KEY, signResultVO, duration);
         } catch (Exception e) {
+            e.printStackTrace(); // 或者使用日志框架记录异常
         } finally {
             // 关闭 OkHttpClient
-            client.dispatcher().executorService().shutdown();
+            client.dispatcher().executorService().shutdownNow();
             client.connectionPool().evictAll();
         }
     }
