f49648bb3972802c7861c7e6c68e04f322628256
刘豪 <ohiyou999@163.com>
2024-03-20 17:59:34 +0800

    解决冲突

--- src/main/java/cloud/ohiyou/Main.java
+++ src/main/java/cloud/ohiyou/Main.java
@@ -21,8 +21,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
-    private static final String COOKIE = System.getenv("COOKIE");
-    //    private static final String COOKIE = "bbs_sid=3q22tlil9nsnlh878ajigcdu49; bbs_token=vTOaYxxcCo3L24s_2FFwbDXnDY50kEhyirM7Zs1bvh_2FeQlNq_2B_2FQ2kjlY00b0IOKYhOUaQ3oi0GidfRoJgcnGaFFCzC5NR6LhoT";
+//    private static final String COOKIE = System.getenv("COOKIE");
+        private static final String COOKIE = "bbs_sid=3q22tlil9nsnlh878ajigcdu49; bbs_token=vTOaYxxcCo3L24s_2FFwbDXnDY50kEhyirM7Zs1bvh_2FeQlNq_2B_2FQ2kjlY00b0IOKYhOUaQ3oi0GidfRoJgcnGaFFCzC5NR6LhoT";
     private static final String DINGTALK_WEBHOOK = System.getenv("DINGTALK_WEBHOOK"); // 钉钉机器人 access_token 的值
     private static final String WXWORK_WEBHOOK = System.getenv("WXWORK_WEBHOOK"); // 企业微信机器人 key 的值
     private static final String SERVER_CHAN_KEY = System.getenv("SERVER_CHAN");
@@ -52,7 +52,7 @@ public class Main {
             // 推送
             publishWechat(SERVER_CHAN_KEY, signResultVO, (endTime - startTime));
             DingTalkUtils.pushBotMessage(DINGTALK_WEBHOOK, signResultVO.getMessage(), "", "markdown"); // 推送钉钉机器人
-            WeChatWorkUtils.pushBotMessage(WXWork_WEBHOOK, signResultVO.getMessage(), "markdown");
+            WeChatWorkUtils.pushBotMessage(WXWORK_WEBHOOK, signResultVO.getMessage(), "markdown");
         } catch (Exception e) {
             e.printStackTrace(); // 或者使用日志框架记录异常
         } finally {
