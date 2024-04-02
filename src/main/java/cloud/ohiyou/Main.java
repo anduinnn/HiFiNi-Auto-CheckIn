058411060e756cb291ad9a13ee472ce010e202ac
autumn <981244680@qq.com>
2024-04-02 22:04:42 +0800

    获取用户名,在推送的时候输出用户名

--- src/main/java/cloud/ohiyou/Main.java
+++ src/main/java/cloud/ohiyou/Main.java
@@ -6,7 +6,6 @@ package cloud.ohiyou;
  */
 
 import cloud.ohiyou.utils.DingTalkUtils;
-import cloud.ohiyou.utils.HiFiNiEncryptUtil;
 import cloud.ohiyou.utils.WeChatWorkUtils;
 import cloud.ohiyou.vo.CookieSignResult;
 import cloud.ohiyou.vo.SignResultVO;
@@ -24,8 +23,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
-        private static final String COOKIE = System.getenv("COOKIE");
-//    private static final String COOKIE = "bbs_sid=7bl67b8516gvcn3qejbcsnvotf; bbs_token=1sslA6_2B2TGqkFbnc8t0HPuvahctVQURp_2B28V13Nx2eSp0oEGkMjNS5x9ZTxHrPzbvXgFzvVdcHrd_2BNU2Ar_2F_2FL63RjCym7mrj &\n";
+//        private static final String COOKIE = System.getenv("COOKIE");
+private static final String COOKIE = "bbs_sid=7bl67b8516gvcn3qejbcsnvotf; bbs_token=1sslA6_2B2TGqkFbnc8t0HPuvahctVQURp_2B28V13Nx2eSp0oEGkMjNS5x9ZTxHrPzbvXgFzvVdcHrd_2BNU2Ar_2F_2FL63RjCym7mrj &\n";
     private static final String DINGTALK_WEBHOOK = System.getenv("DINGTALK_WEBHOOK"); // 钉钉机器人 access_token 的值
     private static final String WXWORK_WEBHOOK = System.getenv("WXWORK_WEBHOOK"); // 企业微信机器人 key 的值
     private static final String SERVER_CHAN_KEY = System.getenv("SERVER_CHAN");
@@ -37,6 +36,9 @@ public class Main {
 
     public static void main(String[] args) {
         List<CookieSignResult> results = new ArrayList<>();
+        if (COOKIE == null) {
+            throw new RuntimeException("未设置Cookie");
+        }
 
         String[] cookiesArray = COOKIE.split("&");
         log("检测到 " + cookiesArray.length + " 个cookie");
@@ -51,7 +53,7 @@ public class Main {
                 if (formattedCookie != null) {
                     SignResultVO signResultVO = sendSignInRequest(formattedCookie);
                     long endTime = System.currentTimeMillis();
-                    results.add(new CookieSignResult(cookieName, signResultVO, endTime - startTime));
+                    results.add(new CookieSignResult(signResultVO, endTime - startTime));
                 }
             } catch (Exception e) {
                 log("处理 " + cookieName + " 时发生错误: " + e.getMessage());
@@ -70,7 +72,7 @@ public class Main {
         boolean allSuccess = true; // 假设所有签到都成功，直到发现失败的签到
 
         for (CookieSignResult result : results) {
-            messageBuilder.append(result.getCookieName()).append(": ")
+            messageBuilder.append(result.getSignResult().getUserName()).append(": ")
                     .append("\n签到结果: ").append(result.getSignResult().getMessage())
                     .append("\n耗时: ").append(result.getDuration()).append("ms\n\n");
             // 检查每个签到结果，如果有失败的，则设置allSuccess为false
@@ -90,8 +92,10 @@ public class Main {
 
 
     private static SignResultVO sendSignInRequest(String cookie) {
-        // 获取Sign
-        String sign = getSignKey(cookie);
+        // 获取签到页面
+        String signPageCode = getSignPage(cookie);
+        String sign = getSign(signPageCode);
+        String userName = getUserName(signPageCode);
         // 获取加密参数
 //        String dynamicKey = HiFiNiEncryptUtil.generateDynamicKey();
 //        String encryptedSign = HiFiNiEncryptUtil.simpleEncrypt(sign, dynamicKey);
@@ -110,14 +114,50 @@ public class Main {
         try (Response response = client.newCall(request).execute()) {
             if (!response.isSuccessful()) {
                 throw new IOException("Unexpected code " + response);
-            };
+            }
+            ;
             String result = readResponse(response);
-            return stringToObject(result, SignResultVO.class);
+            SignResultVO signResultVO = stringToObject(result, SignResultVO.class);
+            signResultVO.setUserName(userName);
+            return signResultVO;
         } catch (IOException e) {
             e.printStackTrace();
         }
         System.out.println();
         return null;
+    }
+
+    private static String getUserName(String signPageCode) {
+        String userName = "未知";
+
+        // 使用正则匹配用户名
+        Pattern pattern = Pattern.compile("<li class=\"nav-item username\"><a class=\"nav-link\" href=\"my.htm\"><img class=\"avatar-1\" src=\".*?\"> (.*?)</a></li>");
+        Matcher matcher = pattern.matcher(signPageCode);
+
+        if (matcher.find()) {
+            userName = matcher.group(1);
+        }
+        return userName;
+    }
+
+    private static String getSign(String signPageCode) {
+        // 通过正则获取sign的值
+        if (signPageCode.contains("请登录")) {
+            throw new RuntimeException("cookie失效");
+        }
+
+        // 使用正则表达式匹配sign变量的值
+        Pattern pattern = Pattern.compile("var sign = \"([^\"]+)\"");
+        Matcher matcher = pattern.matcher(signPageCode);
+
+        if (matcher.find()) {
+            // 如果找到了匹配，提取第一组（括号内的部分）
+            String signValue = matcher.group(1);
+            System.out.println("Sign的值是: " + signValue);
+            return signValue;
+        } else {
+            throw new RuntimeException("未能获取sign,请检查cookie是否失效");
+        }
     }
 
     /**
@@ -126,7 +166,7 @@ public class Main {
      * @param cookie cookie
      * @return String
      */
-    private static String getSignKey(String cookie) {
+    private static String getSignPage(String cookie) {
         // 先携带cookie访问一次签到页面获取sign
         Request request = new Request.Builder()
                 .url("https://www.hifini.com/sg_sign.htm")
@@ -136,25 +176,7 @@ public class Main {
                 .build();
         try (Response response = client.newCall(request).execute()) {
             if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
-            String result = readResponse(response);
-
-            // 通过正则获取sign的值
-            if (result.contains("请登录")) {
-                throw new RuntimeException("cookie失效");
-            }
-
-            // 使用正则表达式匹配sign变量的值
-            Pattern pattern = Pattern.compile("var sign = \"([^\"]+)\"");
-            Matcher matcher = pattern.matcher(result);
-
-            if (matcher.find()) {
-                // 如果找到了匹配，提取第一组（括号内的部分）
-                String signValue = matcher.group(1);
-                System.out.println("Sign的值是: " + signValue);
-                return signValue;
-            } else {
-                throw new RuntimeException("未能获取sign,请检查cookie是否失效");
-            }
+            return readResponse(response);
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/main/java/cloud/ohiyou/vo/CookieSignResult.java
+++ src/main/java/cloud/ohiyou/vo/CookieSignResult.java
@@ -1,23 +1,12 @@
 package cloud.ohiyou.vo;
 
 public class CookieSignResult {
-    // 基于索引的cookie名称
-    private String cookieName;
     private SignResultVO signResult;
     private long duration;
 
-    public CookieSignResult(String cookieName, SignResultVO signResult, long duration) {
-        this.cookieName = cookieName;
+    public CookieSignResult(SignResultVO signResult, long duration) {
         this.signResult = signResult;
         this.duration = duration;
-    }
-
-    public String getCookieName() {
-        return cookieName;
-    }
-
-    public void setCookieName(String cookieName) {
-        this.cookieName = cookieName;
     }
 
     public SignResultVO getSignResult() {
